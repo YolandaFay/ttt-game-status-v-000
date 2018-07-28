b85fcf4cf598b460a61ce6206e6073c766c9b4db
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 20:10:21 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,7 +22,7 @@ def won?(board)
     position_2 = board[windex_2] 
     position_3 = board[windex_3] 
     
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || 
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O") 
     
   end
   
