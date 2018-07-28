ad340c27c1473ce74072d909dd01ec8b1e9a2a5b
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 20:09:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,12 +14,13 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 def won?(board)
   
   WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+    windex_1 = win_combination[0]
+    windex_2 = win_combination[1]
+    windex_3 = win_combination[2]
     
-    position
-    
+    position_1 = board[windex_1] 
+    position_2 = board[windex_2] 
+    position_3 = board[windex_3] 
     
     
     
