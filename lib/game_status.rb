770aee88fffed624370ad05ff4be32ff9652442a
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 19:58:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,11 +6,13 @@ end
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+#board = [ "0", "1", "2", "3", "4", "5", "6", "7", "8"]
 
 def won?(board)
   index = 0
   win_index = 0 
   
+  #row test 
     if board[index] == board[index+1] == board[index+2] 
       return WIN_COMBINATIONS[win_index]
     else
