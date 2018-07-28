e6986a02aaeda924c33e6eb8824563ba4a2b264e
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 20:04:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,6 +9,14 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 #board = [ "0", "1", "2", "3", "4", "5", "6", "7", "8"]
 
 def won?(board)
+  win_counter = 0 
   
+  WIN_COMBINATIONS.each do |win_combo|
+    if board[win_counter] == board[win_counter+1] == board[win_counter+2]
+      return WIN_COMBINATIONS[win_counter]
+      
+    
+    
+  end
   
 end
