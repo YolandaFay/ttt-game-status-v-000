523089f99ca8365cd1d6035c2306d31439c24fa4
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 20:30:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,6 +8,17 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
+  
+    index = 0 
+    empty = false
+    
+    while index <= 9
+       if position_taken?(board, index) == false
+          index += 1 
+        else
+          return empty
+        end
+      end
 
     WIN_COMBINATIONS.each do |win_combo|
     windex_1 = win_combo[0]
