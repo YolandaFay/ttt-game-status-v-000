f025049451c932239049e664efb75a7c6ae8aa57
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 19:41:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 
-win_combinations = Array.new([0,1,2],[2,3,4],[6,7,8],[0,4,8],[3,4,6])
+win_combinations = Array.new([0,1,2],[2,3,4],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[3,4,6])
 
 def won?(board)
   
