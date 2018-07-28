e3a28d9744ff012a01a506e386c3755d2af0a206
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 20:25:52 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,14 +3,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def board_empty?(board)
-  board.each do |entry|
-    if entry != " "
-      return false 
-    end
-  end
-end
-
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
@@ -20,10 +12,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
          #"X", "X", "O"] 
 
 def won?(board)
-  
-  if board_empty?(board) == false
-    return false 
-  else
+
     WIN_COMBINATIONS.each do |win_combo|
     windex_1 = win_combo[0]
     windex_2 = win_combo[1]
@@ -39,5 +28,4 @@ def won?(board)
       false 
     end
   end
-end
 end
