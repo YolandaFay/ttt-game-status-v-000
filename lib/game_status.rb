b7f257a52299892982746eb00bdfd8fed4f9b686
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 20:20:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,13 +12,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
          #"X", "X", "O"] 
 
 def won?(board)
-  
-  board.each do |entry|
-    if entry != " " 
-      return false 
-    end
-  end 
-  
+
   WIN_COMBINATIONS.each do |win_combo|
     windex_1 = win_combo[0]
     windex_2 = win_combo[1]
